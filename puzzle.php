e69eaabdee08870f6128c2ce882d9969ec59598a
Pamela Petterchak <pfpg7m@mst.edu>
2020-05-03 16:41:16 -0500

    bug fix

--- puzzle.php
+++ puzzle.php
@@ -74,7 +74,7 @@
         mysqli_stmt_bind_result($checkFlag, $puzzleValue);
         mysqli_stmt_fetch($checkFlag);
         $checkFlag =  mysqli_prepare($ReaverDB, "UPDATE reaver.team SET score = score + ? WHERE t_name = ?");
-        mysqli_stmt_bind_param($checkFlag, 'ii', $puzzleValue, $puzzlenumber);
+        mysqli_stmt_bind_param($checkFlag, 'is', $puzzleValue, $puzzlenumber);
         $puzzlenumber = $_POST["puzzlenumber"];
         mysqli_stmt_execute($checkFlag);
 
