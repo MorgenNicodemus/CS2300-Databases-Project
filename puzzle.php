304f4f0e014ef80e87bcdb81ce1103aa70d556e3
Pamela Petterchak <pfpg7m@mst.edu>
2020-05-03 16:47:58 -0500

    first attempt at ranking algorithm

--- puzzle.php
+++ puzzle.php
@@ -79,10 +79,10 @@
         mysqli_stmt_execute($checkFlag);
 
         //Adjust team rankings (note- this may need to be done in CTE view, not directly to the table)
-        //mysqli_prepare($ReaverDB, "SELECT t_name, score,
-                                            //RANK() OVER (ORDER BY score DESC) AS t_rank
-                                  // FROM reaver.team
-                                  // UPDATE reaver.team SET 'rank' = t_rank");
+        mysqli_prepare($ReaverDB, "SELECT t_name, score,
+                                            RANK() OVER (ORDER BY score DESC) AS t_rank
+                                   FROM reaver.team
+                                   UPDATE reaver.team SET 'rank' = t_rank");
       } else {
         echo "Flag is Incorrect.";
       }
