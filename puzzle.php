9fd5ce2d34203dad0ca7a873a39307be33bc19d9
Pamela Petterchak <pfpg7m@mst.edu>
2020-05-03 14:09:42 -0500

    changed CSS at line 43 to make puzzles prettier

--- puzzle.php
+++ puzzle.php
@@ -38,8 +38,9 @@
     $result = mysqli_query($ReaverDB, $getPuzzleInfo);
 
     if (mysqli_num_rows($result) > 0) {
+        echo "<h1>Puzzles</h1>"
       while($row = mysqli_fetch_assoc($result)) {
-        echo $row["c_name"] . "\r\n" . $row["puzz_name"] . "\r\n" . $row["puzz_val"];
+        echo "<br>Puzzle Number " .$row["puzz_no"]." | ".$row["c_name"] . " | " . $row["puzz_name"] . " | " . $row["puzz_val"] . "Points | ".$row["puzz_body"].;
       }
     }
 
